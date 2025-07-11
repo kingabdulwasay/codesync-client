2d8785b8acf07efd29381bd270dc63428404daa6
kingabdulwasay <awasay245@gmail.com>
2025-07-11 10:20:39 -0700

    CodeSync-Client-V1

---
+++ signup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('create-btn').addEventListener('click', async() => {
+        const workspaceName = document.getElementById('work').value.trim()
+        const username = document.getElementById('user').value.trim()
+
+
+            localStorage.setItem('workspace', JSON.stringify({username, workspaceName, host: true}))
+            window.location.replace('http://127.0.0.1:5500/client/index.html')
+        
+    })
+        document.getElementById('join-btn').addEventListener('click', async() => {
+        const workspaceName = document.getElementById('work').value.trim()
+        const username = document.getElementById('user').value.trim()
+
+            localStorage.setItem('workspace', JSON.stringify({username, workspaceName,host: false}))
+            window.location.replace('http://127.0.0.1:5500/client/index.html')
+        
+    })
+})
